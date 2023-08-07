4487c4d5273e20665273083134378326b3644b42
Rafdav-400 <rafael.velez.2211@gmail.com>
2023-08-07 09:32:44 -0600

    Added files here

---
+++ js/boolean.js
@@ -0,0 +1,10 @@
+/*alert("Hola Mundo!")*/ 
+
+let user = true;
+let username = "Rafa"
+
+if (user){
+    console.log(`Hola ${username}.`);
+}else{
+    console.log("Hola, quien eres?")
+}
---
+++ js/objetos.js
@@ -0,0 +1,38 @@
+
+//console.log('Objetos')
+
+const producto = {
+    nombre: 'manzana',
+    precio: 5,
+    disponible: true,
+}
+
+const heroe1 = {
+    nombre: 'Spider Man',
+    alterEgo: 'Peter Parker',
+    superPoder: 'Trepar Paredes',
+    altura: '1.8',
+    peso: 100,
+    direccion: '???',
+    contacto: '???',
+}
+
+const heroe2 = {
+    nombre: 'Iron Man',
+    alterEgo: 'Tony Spark',
+    superPoder: 'Trepar Paredes',
+    altura: '1.83',
+    peso: 128,
+    direccion: '???',
+    contacto: '???',
+}
+
+const heroe3 = {
+    nombre: 'Holk/Hulk',
+    alterEgo: 'Bruce Banner',
+    superPoder: 'Fuerza',
+    altura: '~2.89',
+    peso: 450,
+    direccion: '???',
+    contacto: '???',
+}
